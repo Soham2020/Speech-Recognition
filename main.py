5c0a56ae2dff4c1d3d1697868f7c4c74eed9bb86
Soham Das <majumder70.ranu@gmail.com>
2020-09-18 21:23:14 +0530

    hello Jenny

---
+++ main.py
@@ -0,0 +1,147 @@
+import pyttsx3
+import speech_recognition as sr
+import wikipedia
+import os
+import webbrowser
+import smtplib
+import datetime
+# import requests
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[0].id) -> DAVID 
+# print(voices[1].id) -> ZIRA
+engine.setProperty('voice', voices[1].id)
+
+def speak(audio):       #speak function
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning")
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon")
+    else:
+        speak("Good Evening")
+    speak("Welcome Soham Das, I am back on duty. Waiting for your command")
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('sohamdas8697@gmail.com', 'soham8697')
+    server.sendmail('Senderemail@gmail.com', to, content)
+    server.close()
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening.....")
+        speak("I am Listening")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print("Recognizing...")
+        speak("Recognizing")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"Soham said: {query}\n")
+    except Exception as e:
+        # print(e)
+        print("Say that again....")
+        speak("Say that again")
+        return "None"
+    return query
+
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+
+        if 'wikipedia' in query:
+            speak("Searching")
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to wikipedia")
+            speak(results)
+        
+        elif 'tell some jokes' in query:
+            print("Why did the cowboy buy a dachshund? Someone told him to get a long little doggy.")
+            speak("Why did the cowboy buy a dachshund? Someone told him to get a long little doggy.")
+        elif 'tell me something interesting' in query:
+            print("It's estimated that there are 75 quintillion grains of sand on Earth. That's 75 with 17 zeros after it!")
+            speak("It's estimated that there are 75 quintillion grains of sand on Earth. That's 75 with 17 zeros after it!")
+
+        # elif 'how is the weather' and 'weather' in query:
+
+        #     url = 'https://api.openweathermap.org/'#Open api link here
+
+        #     res = requests.get(url)
+
+        #     data = res.json()
+
+        #     weather = data['weather'] [0] ['main'] 
+        #     temp = data['main']['temp']
+        #     wind_speed = data['wind']['speed']
+
+        #     latitude = data['coord']['lat']
+        #     longitude = data['coord']['lon']
+
+        #     description = data['weather'][0]['description']
+        #     speak('Temperature : {} degree celcius'.format(temp))
+        #     print('Wind Speed : {} m/s'.format(wind_speed))
+        #     print('Latitude : {}'.format(latitude))
+        #     print('Longitude : {}'.format(longitude))
+        #     print('Description : {}'.format(description))
+        #     print('weather is: {} '.format(weather))
+        #     speak('weather is : {} '.format(weather))
+
+
+        elif 'open google' in query:
+            speak("opening Google baaba")
+            webbrowser.open("google.com")
+        elif 'open youtube' in query:
+            speak("opening youtube")
+            webbrowser.open("youtube.com")
+        elif 'open geeks for geeks' in query:
+            speak("opening geeksforgeeks")
+            webbrowser.open("geeksforgeeks.org")
+
+        elif 'time' in query:
+            strTime = datetime.datetime.now().strftime("%I:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'introduce' in query:
+            speak("I am Jenny 1.0")
+            speak("I am designed by Soham Das in his room, as he was very depressed, so he built me, to share his feelings, and i am happy with it")
+            speak("i am always ready to help anyone, in return of love")
+
+        elif 'open vs code' in query:
+            speak("Opening visual,studio,code")
+            codePath="D:\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+        elif 'open dev c' in query:
+            speak("opening c++, and try to complete atleast 10 problems")
+            codePath="D:\\Dev-Cpp\\devcpp.exe"
+            os.startfile(codePath)
+        elif 'how are you' in query:
+            speak("I am fine!!")
+            speak("What about you?")
+            # print("What about you?")
+        
+        elif 'email to shubham' and 'send email' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "shubham.jha2001m@gmail.com"    
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry my friend . I am not able to send this email")  
+            
+        elif 'sleep' in query:
+            speak("ok sir shutting down the system")
+            quit()
