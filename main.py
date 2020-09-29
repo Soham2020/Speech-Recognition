00e3fd8c11efe515d2e742aaf88bbebeeede545f
Soham Das <66131928+Soham2020@users.noreply.github.com>
2020-09-29 21:57:14 +0530

    jenny 1.1

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import webbrowser
 import smtplib
 import datetime
 import random
-# import requests
+import requests
+import pywhatkit as kit
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -20,6 +21,10 @@ emaildict = {
                 "sourasish" : "bppcs.11500119119@gmail.com", 
                 "swarnava" : "swarnavadas2014@gmail.com"
             }
+# whatsapp = {
+#     "Suraj" : "+918240509286",
+#     "subhrakanti" : "+917044124579"
+# }
 def speak(audio):       #speak function
     engine.say(audio)
     engine.runAndWait()
@@ -38,7 +43,7 @@ def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
-    server.login('sohamdas8697@gmail.com', 'PASSWORD')
+    server.login('sohamdas8697@gmail.com', 'soham8697')
     server.sendmail('sohamdas8697@gmail.com', to, content)
     server.close()
 
@@ -75,37 +80,34 @@ if __name__ == "__main__":
             speak(results)
         
      
-        # elif 'how is the weather' and 'weather' in query:
-
-        #     url = 'https://api.openweathermap.org/'#Open api link here
-
-        #     res = requests.get(url)
-
-        #     data = res.json()
-
-        #     weather = data['weather'] [0] ['main'] 
-        #     temp = data['main']['temp']
-        #     wind_speed = data['wind']['speed']
-
-        #     latitude = data['coord']['lat']
-        #     longitude = data['coord']['lon']
-
-        #     description = data['weather'][0]['description']
-        #     speak('Temperature : {} degree celcius'.format(temp))
-        #     print('Wind Speed : {} m/s'.format(wind_speed))
-        #     print('Latitude : {}'.format(latitude))
-        #     print('Longitude : {}'.format(longitude))
-        #     print('Description : {}'.format(description))
-        #     print('weather is: {} '.format(weather))
-        #     speak('weather is : {} '.format(weather))
         elif 'weather' in query:
-            area = webbrowser.open("https://www.google.com/search?q=weather")
-            speak("Weather forcast of area is")
+
+            url = 'http://api.openweathermap.org/data/2.5/weather?q=Kolkata&appid=84996683165caf523477d96ede6636cd'#Open api link here
+
+            res = requests.get(url)
+
+            data = res.json()
+
+            weather = data['weather'] [0] ['main'] 
+            temp = data['main']['temp']
+            wind_speed = data['wind']['speed']
+
+            latitude = data['coord']['lat']
+            longitude = data['coord']['lon']
+
+            description = data['weather'][0]['description']
+            speak('Temperature : {} degree celcius'.format(temp))
+            print('Wind Speed : {} m/s'.format(wind_speed))
+            print('Latitude : {}'.format(latitude))
+            print('Longitude : {}'.format(longitude))
+            print('Description : {}'.format(description))
+            print('weather is: {} '.format(weather))
+            speak('weather is : {} '.format(weather))
 
         elif 'search' in query:
-            query=query.replace("search","")
-            speak("opening Google baaba")
-            webbrowser.open("https://www.google.com/search?q="+query)
+            speak("Sir, What should I search")
+            cm = takeCommand().lower()
+            webbrowser.open(f"{cm}")
         elif 'play' in query:
             query=query.replace("play", "")
             speak("opening youtube")
@@ -121,6 +123,7 @@ if __name__ == "__main__":
             # print(f"Sir, the date is {strDate}")
             speak(f"Sir, the time is {strTime}")
             # speak(f"Sir, the date is {strDate}")
+            
 #introduction and chats
         elif 'hello' in query:
             speak("I am Jenny 1.0")
@@ -170,6 +173,14 @@ if __name__ == "__main__":
             except Exception:
                 speak("Sorry! I am not able to send this email")
                 print("Sorry! I am not able to send this email")
+
+        elif 'message' in query:
+            speak("What should I say?")
+            print("What should I say?")
+            content = takeCommand()
+            kit.sendwhatmsg("+919830920321", content, 19,4)
+            speak("Message had been sent successfully")
+
         
         elif 'tell some jokes' in query:
             print("Why did the cowboy buy a dachshund? Someone told him to get a long little doggy.")
